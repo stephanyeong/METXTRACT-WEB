56324788e1949997f2dd779c5d3f4a69aa0db308
aprillejoy <125043764+aprillejoytabafunda@users.noreply.github.com>
2023-12-08 00:44:43 +0800

    new logo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/NavigationBar.css';
 import {
   MdOutlineLocationOn,
@@ -10,12 +10,34 @@ import { FaFacebookF, FaInstagram, FaXTwitter } from 'react-icons/fa6';
 
 function NavigationBar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <nav className="navigation">
-      <a href="/" className="brand-name">
-        METXTRACT
-      </a>
+      <div className="brand-container">
+        {isMobile ? (
+          <img src="images/logo.png" alt="Logo" className="logo-image" />
+        ) : (
+          <>
+            <a href="/" className="brand-name">
+              METXTRACT
+            </a>
+            <img src="images/logo.png" alt="Logo" className="logo-image-web" />
+          </>
+        )}
+      </div>
       <button
         className="hamburger"
         onClick={() => {
